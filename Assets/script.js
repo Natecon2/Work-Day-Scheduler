b20d6303b7e58ff98a7861909f0a37e3afeb294c
Natecon2 <frankenfieldnathan5@gmail.com>
2023-07-10 14:58:16 -0700

    added comments to the Javascript, changed name of develop folder to Assets, removed index.html from Assets folder.

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,8 @@
  $(function () {
+  // Function to update the colors of time-blocks based on the current hour
     function updateTimeBlockColors() {
       var currentHour = dayjs().format("H");
-  
+ 
       $(".time-block").each(function () {
         var timeBlockId = parseInt($(this).attr("id").split("-")[1]);
   
@@ -14,7 +15,7 @@
         }
       });
     }
-  
+  // Function to load saved events from local storage and populate the textareas
     function loadSavedEvents() {
       $(".time-block").each(function () {
         var timeBlockId = $(this).attr("id");
@@ -25,21 +26,21 @@
         }
       });
     }
-  
+  // Function to save the user input for a time-block in local storage
     function saveEvent() {
       var timeBlockId = $(this).parent().attr("id");
       var userInput = $(this).siblings(".description").val();
   
       localStorage.setItem(timeBlockId, userInput);
     }
-  
+  // Function to display the current date in the header
     function displayCurrentDate() {
       var currentDate = dayjs().format("dddd, MMMM D, YYYY");
       $("#currentDay").text(currentDate);
     }
-  
+  // Event listener for save buttons
     $(".saveBtn").on("click", saveEvent);
-  
+  // Call the functions to initialize the page
     updateTimeBlockColors();
     loadSavedEvents();
     displayCurrentDate();
